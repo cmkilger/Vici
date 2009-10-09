9f7e0babd5dcd37866500c5e3033b91f3c78b1b3
Cory Kilger <cmkilger@gmail.com>
2009-10-09 06:07:54 +0000

    Created Xcode project, including 1st draft of the Core Data model as discussed during the October meeting.
    new review

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BYUCHProject
+//
+//  Created by Cory Kilger on 10/8/09.
+//  Copyright Cory Kilger 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
