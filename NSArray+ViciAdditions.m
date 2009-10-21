36346cb626efd06c54d2997d28dfa49b9b69de37
Cory Kilger <cmkilger@gmail.com>
2009-10-21 04:35:44 +0000

    Implemented rules logic (Triggers, Conditions, Actions, ActionParameters).

---
+++ NSArray+ViciAdditions.m
@@ -0,0 +1,26 @@
+//
+//  NSArray+ViciAdditions.m
+//  Vici
+//
+//  Created by Cory Kilger on 10/20/09.
+//  Copyright 2009 Cory Kilger. All rights reserved.
+//
+
+#import "NSArray+ViciAdditions.h"
+
+
+@implementation NSArray (ViciAdditions)
+
+- (NSArray *) sortedArrayUsingDescriptor:(NSSortDescriptor *)sortDescriptor {
+	return [self sortedArrayUsingDescriptors:[NSArray arrayWithObject:sortDescriptor]];
+}
+
+@end
+
+@implementation NSSet (ViciAdditions)
+
+- (NSArray *) sortedArrayUsingDescriptor:(NSSortDescriptor *)sortDescriptor {
+	return [self sortedArrayUsingDescriptors:[NSArray arrayWithObject:sortDescriptor]];
+}
+
+@end
