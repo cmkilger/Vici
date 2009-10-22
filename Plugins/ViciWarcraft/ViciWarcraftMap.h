895901b99b313208dd0bc6750c476a10b9882e27
Cory Kilger <cmkilger@gmail.com>
2009-10-22 00:14:35 +0000

    Added ViviWarcraft plugin.  Not fully implemented.

---
+++ Plugins/ViciWarcraft/ViciWarcraftMap.h
@@ -0,0 +1,16 @@
+//
+//  ViciWarcraftMap.h
+//  Vici
+//
+//  Created by Cory Kilger on 10/21/09.
+//  Copyright 2009 Cory Kilger. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "ViciCore.h"
+
+@interface ViciWarcraftMap : ViciPlugin {
+
+}
+
+@end
---
+++ Plugins/ViciWarcraft/ViciWarcraftMap.m
@@ -0,0 +1,14 @@
+//
+//  ViciWarcraftMap.m
+//  Vici
+//
+//  Created by Cory Kilger on 10/21/09.
+//  Copyright 2009 Cory Kilger. All rights reserved.
+//
+
+#import "ViciWarcraftMap.h"
+
+
+@implementation ViciWarcraftMap
+
+@end
