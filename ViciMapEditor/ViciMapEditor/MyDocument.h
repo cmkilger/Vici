e290574dcac0222f3a2c2ee10c47c1e3880decf2
Robert Brown <tyranitar207@aol.com>
2009-12-08 01:32:45 +0000

    Imported the ViciMapEditor.  Only the UI has been defined.

---
+++ ViciMapEditor/ViciMapEditor/MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  ViciMapEditor
+//
+//  Created by Robert Brown on 12/3/09.
+//  Copyright 2009 BYU. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ ViciMapEditor/ViciMapEditor/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ViciMapEditor
+//
+//  Created by Robert Brown on 12/3/09.
+//  Copyright 2009 BYU. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
